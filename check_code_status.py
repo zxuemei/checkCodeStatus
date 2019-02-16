f2af75b00d5d9e058a74b7af22671f3d6538149e
zhangxuemei <474442193@qq.com>
2019-02-16 14:48:06 +0800

    modified v02

--- check_code_status.py
+++ check_code_status.py
@@ -1,29 +1,22 @@
-# coding=utf-8
-import sys
 import subprocess
-
-
-def getUserResult(key):
-     answer=input(key)
-     return answer
-
+import sys
 
 def check_code_git_status():
     result = subprocess.getstatusoutput('git status')
-    if result[0] >> 8 != 0:
-        print("Get git statuts fail")
-        sys.exit(1);
-    else:
-        committype = getUserResult('编译正式软件发现本地代码有差异： 是否过滤掉，过滤（yes），停止（no）:')
-        if not (committype == 'yes' or committype == 'no'):
-            print("\033[7;31m输入错误，请重新选择\033[0m")
+    if result[0]>>8 != 0:
+        print('git statu Failed! pls check your git')
+        sys.exit(0)
+    elif 'Changes not staged for commit:' in result[1]:
+        answer = input('编译正式软件发现本地代码有差异，是否要过滤掉？过滤(yes),停止(no)：')
+        if not (answer == 'yes' or answer == 'no'):
+            print('\033[7;31m 选择错误，中断/033[0m')
             sys.exit(0)
-        elif not (committype == 'yes'):
-            print("\033[7;31m本地代码与服务器有差异，您已停止编译\033[0m")
-            sys.exit(0)
-        else:
-            print("\033[7;31m本地代码与服务器有差异，但您选择忽略，故继续编译\033[0m")
+        elif answer == 'yes':
+            print('\033[7;31m 选择过滤，继续编译')
             sys.exit(1)
+        else:
+            print('\033[7;31m 选择停止，停止编译')
+            sys.exit(0)
 
 def main():
     check_code_git_status()
